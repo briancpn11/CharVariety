08367a280cdecf417e9fbfa3494841a69914ba0a
briancpn11 <pingngai@gmail.com>
2021-09-10 22:45:54 +0100

    fixed spacing, added multithreading

--- charVariety/main/mainScript.cpp
+++ charVariety/main/mainScript.cpp
@@ -10,7 +10,7 @@
 #define r 0.01
 #define rr 0.01
 
-int n = 16, nIter[] = {5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5}, tot = 0;
+int nMap = 16, nIter[] = {5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5}, tot = 0;
 int maps[][5] ={{0, 0, 0, 0, 1}, 
                 {0, 0, 0, 1, 1}, 
                 {0, 0, 1, 1, 1}, 
@@ -34,106 +34,94 @@ int maps[][5] ={{0, 0, 0, 0, 1},
 FILE *ofp = fopen("result/minAvgExpansionGrid.txt", "a");
 
 int main(){
-	double k = 3.99, c = 10000000000000;
-	double ans_x, ans_y, ans_z, rt[10], cand, tem;
-	int i_bound = 4.0/r, thread_id;
-	Matrix act[50];
+    double k = 3.99, c = 10000000000000;
+    double ans_x, ans_y, ans_z, m_z[10], cand, tem;
+    Matrix act[50];
 
-	
-	time_t tttt;
-	time(&tttt);
-	printf("Program started on: %s", ctime(&tttt));
-	
-	clock_t time_taken;
-	
-	time_taken = clock();
+    time_t tttt;
+    time(&tttt);
+    printf("Program started on: %s", ctime(&tttt));
 
-	for (int ii = 0; ii < i_bound; ii++){
-		double i = double(ii)*r - 2;
-		for (double j = -2; j < 2; j += r){
-			double st = i*i*j*j - 4*(i*i + j*j - k);
-			if (st > 0.000001){
-				st = sqrt(st);
-				rt[0] = (i*j + st) * 0.5;
-				rt[1] = (i*j - st) * 0.5;
-				for (int s = 0; s < 2; s++){
-					double A = 2*i - j*rt[s], B = 2*j - i*rt[s], C = 2*rt[s] - i*j;
-					if (compMax(C, A, B)){
-						double x = i, 		y = j, 		z = rt[s];
-						double vv1 = 0, 	vv2 = C, 	vv3 = -B;
-						double ww1 = -C, 	ww2 = 0, 	ww3 = A;
-						for (int ss = 0; ss < 3; ss++){	
-							bool bad_count = false;		
-							
-							for (int map_id = 0; map_id < n; map_id++){
-								
-								Pair pr, ps;
-								pr.x = x; pr.y = y; pr.z = z; pr.v1 = vv1; pr.v2 = vv2; pr.v3 = vv3; 
-								ps.x = x; ps.y = y; ps.z = z; ps.v1 = ww1; ps.v2 = ww2; ps.v3 = ww3;
-								
-								pr = action(pr, maps[map_id], nIter[map_id]);
-								ps = action(ps, maps[map_id], nIter[map_id]);
-																
-								double AA = 2*(pr.x) - (pr.y)*(pr.z), BB = 2*(pr.y) - (pr.x)*(pr.z), CC = 2*(pr.z) - (pr.x)*(pr.y);
-								
-								if (compMax(CC, AA, BB)){
-									double sst = 1 / sqrt(abs(C * CC));
-									act[map_id].p11 = pr.v2 * sst;
-									act[map_id].p21 = - pr.v1 * sst;
-									act[map_id].p12 = ps.v2 * sst;
-									act[map_id].p22 = - ps.v1 * sst;
-								} 
-								else if (compMax(BB, AA, CC)){
-									double sst = 1 / sqrt(abs(B * BB));
-									act[map_id].p11 = pr.v1 * sst;
-									act[map_id].p21 = - pr.v3 * sst;
-									act[map_id].p12 = ps.v1 * sst;
-									act[map_id].p22 = - ps.v3 * sst;
-								} 
-								else {
-									double sst = 1 / sqrt(abs(A * AA));
-									act[map_id].p11 = pr.v3 * sst;
-									act[map_id].p21 = - pr.v2 * sst;
-									act[map_id].p12 = ps.v3 * sst;
-									act[map_id].p22 = - ps.v2 * sst;
-								}
-							}
-							
-							cand = minAvgExpansionNewton(act, n);
-							if (c > cand){
-								c = cand;
-								ans_x = x;
-								ans_y = y;
-								ans_z = z;
-							}
-							tot++;
+    clock_t time_taken;
+
+    time_taken = clock();
+
+    int thread_id, ii, i_bound=4.0/r; double t1, t2;
+    #pragma omp parallel private(thread_id, t1, t2) num_threads(4)
+    {
+        t1 = omp_get_wtime();
+        #pragma omp for private(m_z, cand, tem, act) reduction(min: c) reduction(+: tot) nowait
+        for (ii = 0; ii < i_bound; ii++){
+            double m_x = double(ii)*r - 2;
+        // for (double m_x = -2; m_x < 2; m_x += r){
+            for (double m_y = -2; m_y < 2; m_y += r){
+                double disc = sqr(m_x*m_y) - 4*(sqr(m_x) + sqr(m_y) - k);
+                if (disc > EPS){
+                    disc = sqrt(disc);
+                    m_z[0] = (m_x*m_y + disc) * 0.5;
+                    m_z[1] = (m_x*m_y - disc) * 0.5;
+                    for (int s = 0; s < 2; s++){
+                        double A = 2*m_x - m_y*m_z[s], B = 2*m_y - m_x*m_z[s], C = 2*m_z[s] - m_x*m_y;
+                        if (compMax(C, A, B)){
+                            double x = m_x, 	y = m_y, 	z = m_z[s];
+                            double vv1 = 0, 	vv2 = C, 	vv3 = -B;
+                            double ww1 = -C, 	ww2 = 0, 	ww3 = A;
+                            for (int ss = 0; ss < 3; ss++){						
+                                for (int map_id = 0; map_id < nMap; map_id++){
+                                    Pair pr(x, y, z, vv1, vv2, vv3), ps(x, y, z, ww1, ww2, ww3);
+                                    pr = action(pr, maps[map_id], nIter[map_id]);
+                                    ps = action(ps, maps[map_id], nIter[map_id]);
+                                                                    
+                                    double AA = 2*(pr.x) - (pr.y)*(pr.z), BB = 2*(pr.y) - (pr.x)*(pr.z), CC = 2*(pr.z) - (pr.x)*(pr.y);
+                                    if (compMax(CC, AA, BB)){
+                                        double sst = 1 / sqrt(std::abs(C * CC));
+                                        act[map_id] = {pr.v2 * sst, ps.v2 * sst, -pr.v1 * sst, -ps.v1 * sst};
+                                    } 
+                                    else if (compMax(BB, AA, CC)){
+                                        double sst = 1 / sqrt(std::abs(B * BB));
+                                        act[map_id] = {pr.v1 * sst, ps.v1 * sst, -pr.v3 * sst, -ps.v3 * sst};
+                                    } 
+                                    else {
+                                        double sst = 1 / sqrt(std::abs(A * AA));
+                                        act[map_id] = {pr.v3 * sst, ps.v3 * sst, -pr.v2 * sst, -ps.v2 * sst};
+                                    }
+                                }
+                                cand = minAvgExpansionNewton(act, nMap);
+                                if (c > cand){
+                                    c = cand; ans_x = x; ans_y = y; ans_z = z;
+                                }
+                                tot++;
+        
+                                tem = z; z = y; y = x; x = tem;
+                                tem = vv3; vv3 = vv2; vv2 = vv1; vv1 = tem;
+                                tem = ww3; ww3 = ww2; ww2 = ww1; ww1 = tem;	
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        t2 = omp_get_wtime();
+        thread_id = omp_get_thread_num();
+        printf("Thread: %d, Time taken: %.3f sec\n", thread_id, t2 - t1);
+    }
 	
-							tem = z; z = y; y = x; x = tem;
-							tem = vv3; vv3 = vv2; vv2 = vv1; vv1 = tem;
-							tem = ww3; ww3 = ww2; ww2 = ww1; ww1 = tem;	
-						}
-					}
-				}
-			}
-		}
-	}
-	
-	time_taken = clock() - time_taken;
-	double time_taken_sec = (double)time_taken / CLOCKS_PER_SEC;
-	
-	time_t ttt;
-	time(&ttt);
-	
-	fprintf(ofp, "Program run on: %s", ctime(&ttt));
-	fprintf(ofp, "Minimum average expansion: %f\n", c);
-	fprintf(ofp, "Other parameters: \n");
-	fprintf(ofp, " k-2: %f\n", k-2);
-	fprintf(ofp, " Spacing in the surface directions: r: %f\n", r);
-	fprintf(ofp, " Spacing in the tangent direction: rr: %f\n", rr);
-	fprintf(ofp, " Total number of grid points on the surface: %d\n", tot);
-	fprintf(ofp, "The point where the min average expansion occur: P = (%f, %f, %f)\n", ans_x, ans_y, ans_z);
-	fprintf(ofp, "Time taken: %f seconds\n", time_taken_sec);
-	// fprintf(ofp, "cMax = %d, bad_coMax = %d\n", cMax, bad_coMax);
-	fprintf(ofp, "\n\n\n");
-	return 0;
+    time_taken = clock() - time_taken;
+    double time_taken_sec = (double)time_taken / CLOCKS_PER_SEC;
+
+    time_t ttt;
+    time(&ttt);
+
+    fprintf(ofp, "Program run on: %s", ctime(&ttt));
+    fprintf(ofp, "Minimum average expansion: %f\n", c);
+    fprintf(ofp, "Other parameters: \n");
+    fprintf(ofp, " k-2: %f\n", k-2);
+    fprintf(ofp, " Spacing in the surface directions: r: %f\n", r);
+    fprintf(ofp, " Spacing in the tangent direction: rr: %f\n", rr);
+    fprintf(ofp, " Total number of grid points on the surface: %d\n", tot);
+    fprintf(ofp, "The point where the min average expansion occur: P = (%f, %f, %f)\n", ans_x, ans_y, ans_z);
+    fprintf(ofp, "Time taken: %f seconds\n", time_taken_sec);
+    // fprintf(ofp, "cMax = %d, bad_coMax = %d\n", cMax, bad_coMax);
+    fprintf(ofp, "\n\n\n");
+return 0;
 }
--- charVariety/utils/dataStructure.h
+++ charVariety/utils/dataStructure.h
@@ -12,25 +12,25 @@
 // define Interval
 
 struct full_rounding:
-  	boost::numeric::interval_lib::rounded_arith_opp<double>
+    boost::numeric::interval_lib::rounded_arith_opp<double>
 {
-	public:
-	# define GENR_FUNC(name) \
-		double name##_down(double x) { return name(x); } \
-		double name##_up  (double x) { return name(x); }
-		GENR_FUNC(exp)
-		GENR_FUNC(log)
-		GENR_FUNC(sin)
-		GENR_FUNC(cos)
+    public:
+    # define GENR_FUNC(name) \
+        double name##_down(double x) { return name(x); } \
+        double name##_up  (double x) { return name(x); }
+        GENR_FUNC(exp)
+        GENR_FUNC(log)
+        GENR_FUNC(sin)
+        GENR_FUNC(cos)
 };
 
 namespace IntervalNS {
-	using namespace boost;
-	using namespace numeric;
-	using namespace interval_lib;
-	typedef save_state<full_rounding> R;
-	typedef checking_strict<double> P;
-	typedef interval<double, policies<R, P> > Interval;
+    using namespace boost;
+    using namespace numeric;
+    using namespace interval_lib;
+    typedef save_state<full_rounding> R;
+    typedef checking_strict<double> P;
+    typedef interval<double, policies<R, P> > Interval;
 };
 
 typedef IntervalNS::Interval Interval;
@@ -51,7 +51,7 @@ typedef IntervalNS::Interval Interval;
 // double sqr(double);
 
 inline double sqr(double a){
-  	return a*a;
+    return a*a;
 }
 
 inline bool compMax(double a, double b, double c){
@@ -87,29 +87,29 @@ class Matrix{
     public:
         // a 2x2 matrix [[p11, p12], [p21, p22]] 
         double p11, p12, 
-               p21, p22;
+                p21, p22;
 
         Matrix(double p11_=1, double p12_=0, double p21_=0, double p22_=1){
-			p11 = p11_; p12 = p12_;
-			p21 = p21_; p22 = p22_;
+            p11 = p11_; p12 = p12_;
+            p21 = p21_; p22 = p22_;
         }
 
         void printMatrix(){
-			std::cout << "M = [[" << p11 << ", " << p12 << "]," << std::endl;
-			std::cout << "     [" << p21 << ", " << p22 << "]]" << std::endl;
+            std::cout << "M = [[" << p11 << ", " << p12 << "]," << std::endl;
+            std::cout << "     [" << p21 << ", " << p22 << "]]" << std::endl;
         }
 
         bool operator==(Matrix const &B){
-          	return (p11 == B.p11) && (p12 == B.p12) && (p21 == B.p21) && (p22 == B.p22);
+            return (p11 == B.p11) && (p12 == B.p12) && (p21 == B.p21) && (p22 == B.p22);
         }
 
         // overload multiplication for matrices
         Matrix operator*(Matrix const &B){
-			Matrix C(p11 * B.p11 + p12 * B.p21, 
-					 p11 * B.p12 + p12 * B.p22, 
-					 p21 * B.p11 + p22 * B.p21, 
-					 p21 * B.p12 + p22 * B.p22);
-			return C;
+            Matrix C(p11 * B.p11 + p12 * B.p21, 
+                        p11 * B.p12 + p12 * B.p22, 
+                        p21 * B.p11 + p22 * B.p21, 
+                        p21 * B.p12 + p22 * B.p22);
+            return C;
         }
 
         double coeff0(){ return 0.5 * (sqr(p11) + sqr(p21) + sqr(p12) + sqr(p22)); }
@@ -118,34 +118,34 @@ class Matrix{
 
         // treating the 2x2 matrix as a linear transformation, how much does the direction (angle) get expanded?
         double expansionSq(double angle){
-			// expansionSq(angle) = coeff0 + coeff1 * cos(2*angle) + coeff2 * sin(2*angle)
-			return sqr(p11 * cos(angle) + p12 * sin(angle)) + sqr(p21 * cos(angle) + p22 * sin(angle));
+            // expansionSq(angle) = coeff0 + coeff1 * cos(2*angle) + coeff2 * sin(2*angle)
+            return sqr(p11 * cos(angle) + p12 * sin(angle)) + sqr(p21 * cos(angle) + p22 * sin(angle));
         }
 
         // return the contracting direction (angle) of A in the range [0, pi]
         double contractDir(){
-			double c1 = coeff1();
-			double c2 = coeff2();
-			
-			double angle1 = std::abs(c1)<EPS ? PI/2 : atan(c2/c1);
-			double angle2 = angle1 + PI;
-			angle1 = angle1<0 ? angle1+2*PI : angle1;
-			double f1 = c1*cos(angle1) + c2*sin(angle1);
-			double f2 = c1*cos(angle2) + c2*sin(angle2);
+            double c1 = coeff1();
+            double c2 = coeff2();
+            
+            double angle1 = std::abs(c1)<EPS ? PI/2 : atan(c2/c1);
+            double angle2 = angle1 + PI;
+            angle1 = angle1<0 ? angle1+2*PI : angle1;
+            double f1 = c1*cos(angle1) + c2*sin(angle1);
+            double f2 = c1*cos(angle2) + c2*sin(angle2);
 
-			double angle = f1>f2 ? angle2 : angle1;
-			return 0.5 * angle;
+            double angle = f1>f2 ? angle2 : angle1;
+            return 0.5 * angle;
         }
 };
 
 class RotationMatrix: public Matrix{
   	public:
-    	RotationMatrix(double t): Matrix(cos(t), -sin(t), sin(t), cos(t)){}
+        RotationMatrix(double t): Matrix(cos(t), -sin(t), sin(t), cos(t)){}
 };
 
 class DiagonalMatrix: public Matrix{
-  	public:
-    	DiagonalMatrix(double lambda): Matrix(lambda, 0, 0, 1/lambda){}
+    public:
+        DiagonalMatrix(double lambda): Matrix(lambda, 0, 0, 1/lambda){}
 };
 
 #endif
